ed2569986bd6ce1a206d9d475e8ddd0973a7fce0
Blessed-Brighton Chitamba <chtble001@nightmare.cs.uct.ac.za>
2019-04-27 22:30:20 +0200

    first skeleton implementation of imageops driver program. methods not yet implemented

---
+++ imageops.cpp
@@ -0,0 +1,87 @@
+/*the driver file for the imageops application
+  It will do the reading of image file, loading of the image array,
+  as well as taking user input for image processing options
+*/
+
+#include <fstream>  // Needed for file stream objects
+#include <iostream> // Needed for console stream objects
+#include <sstream>  // Needed for string stream objects
+#include <string>   // Needed for strings
+#include <vector>   // Needed for the vector container
+#include "Image.h"  // the file containing the functions
+
+using namespace std;
+using namespace chtble001;
+
+unsigned char* loadImage(string inputFile);
+
+int main(int argc, char * argv[])
+{
+	//process user options
+	//-a, -s, and -l have image1 and image2. -i only has image. -t has image and int f
+	string option = argv[1];
+	
+	//pointers to unsigned char arrays that holds image data
+	unsigned char *data1;
+	unsigned char *data2;
+	
+	//if statement
+	if (option == "-i") {
+		//implement invert
+		string imageFile = argv[2];
+		string outputFile = argv[3];
+		
+		//load data array
+		data1 = loadImage(imageFile);
+		
+		//create the Image object
+		
+	}
+	else if (option == "-t") {
+		//threshhold operation. 
+		string imageFile = argv[2];
+		string threshInt = argv[3];
+		string outputFile = argv[4];
+		
+		//load data array
+		data1 = loadImage(imageFile);
+		
+		//create Image object
+		
+		//call the appropriate method
+	}
+	else {		//else the rest have two image arguments
+		string image1 = argv[2];
+		string image2 = argv[3];
+		string outputFile = argv[4];
+		
+		//load arrays
+		data1 = loadImage(image1);
+		data2 = loadImage(image2);
+		
+		//create Image object
+		
+		if (option == "-a") {
+			//call appropriate method
+			cout << "Option called is " << option << endl;
+		}
+		else if (option == "-s") {
+			//call appropriate method
+			cout << "Option called is " << option << endl;
+		}
+		else if (option == "-l") {
+			//call approptiate method
+			cout << "Option called is " << option << endl;
+		}
+		else {
+			cout << "Invalid option!!" << endl;
+		}
+	}
+}
+
+//loadImage definition
+unsigned char* loadImage(string inputFile) {
+	//implement here
+	unsigned char *imageData = new unsigned char[256];
+	return imageData;
+}
