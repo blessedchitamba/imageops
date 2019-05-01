ecab0614d6aa48a88bc11e0bb5229624cf8aaa05
Blessed-Brighton Chitamba <chtble001@nightmare.cs.uct.ac.za>
2019-05-01 07:54:48 +0200

    added first implementations of load and save functions. still to test them

--- imageops.cpp
+++ imageops.cpp
@@ -14,6 +14,8 @@ using namespace std;
 using namespace chtble001;
 
 unsigned char* loadImage(string inputFile);
+bool saveOutput(const unsigned char *array, string outputFile, int width, int height);
+int Nrows, Ncols;
 
 int main(int argc, char * argv[])
 {
@@ -28,17 +30,22 @@ int main(int argc, char * argv[])
 	//if statement
 	if (option == "-i") {
 		//implement invert
+		cout << "Invert option selected.." << endl;
 		string imageFile = argv[2];
 		string outputFile = argv[3];
 		
 		//load data array
 		data1 = loadImage(imageFile);
 		
+		//to test, write that same array to outputfile
+		saveOutput(data1, outputFile, Nrows, Ncols);
+		
 		//create the Image object
 		
 	}
 	else if (option == "-t") {
 		//threshhold operation. 
+		cout << "Threshhold option selected.." << endl;
 		string imageFile = argv[2];
 		string threshInt = argv[3];
 		string outputFile = argv[4];
@@ -82,6 +89,58 @@ int main(int argc, char * argv[])
 //loadImage definition
 unsigned char* loadImage(string inputFile) {
 	//implement here
-	unsigned char *imageData = new unsigned char[256];
+	cout << "Beginning loadImage.." << endl;
+	unsigned char *imageData;
+	
+	//read from file line by line. Discard header by looping
+	string line;
+	string data;
+	ifstream theFile(inputFile, ios::binary);
+	
+	if(theFile.is_open()) {
+		//first line with P5
+		getline(theFile, line);
+		
+		//loop through comments till the next line that doesnt start with #
+		getline(theFile, line);
+		while(line[0] == '#') {
+			getline(theFile, line);
+		}
+		
+		//current line now is the one that has Nrows and Ncols
+		istringstream iss;
+		iss.str(line);
+		string x, y;
+		iss >> x >> y;
+		Nrows = stoi(x);
+		Ncols = stoi(y);
+		cout << "Nrows is " << Nrows << "and Ncols is " << Ncols << endl;
+		
+		//skip the 255 line
+		getline(theFile, line);
+		
+		//now read the image data
+		imageData = new unsigned char[Nrows*Ncols];
+		theFile.read((char*)imageData, Nrows*Ncols);
+		cout << "Image read successfully." << endl;
+	}
 	return imageData;
+}
+
+//saveOutput definition. takes in a ref to unsigned char array and string filename
+bool saveOutput(const unsigned char *array, string outputFile, int width, int height) {
+	//write the contents of array into the output file as binary
+	
+	ofstream outfile(outputFile, ios::binary);
+	
+	//write header first
+	outfile << "P5" << '\n' << "#output file" << '\n' <<
+		width << height << '\n' << 255 << endl;
+	
+	//now write the array
+	outfile.write((char*)array, width*height);
+	
+	outfile.close();
+	
+	cout << "File with output image created" << endl;
 }
